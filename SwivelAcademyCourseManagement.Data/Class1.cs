5d808b54684a39c36bc1b501c09213bd4677f4af
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2021-05-20 22:06:24 +0100

    Add project files.

---
+++ SwivelAcademyCourseManagement.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SwivelAcademyCourseManagement.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ SwivelAcademyCourseManagement.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SwivelAcademyCourseManagement.Domain
+{
+    public class Class1
+    {
+    }
+}
