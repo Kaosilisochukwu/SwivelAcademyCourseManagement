ad26498ebd1be6baad843477fa77f3b4ed83b509
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2021-05-22 20:30:32 +0100

    Add mapping for objects

---
+++ SwivelAcademyCourseManagement.Domain/Maps/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SwivelAcademyCourseManagement.Domain.DTOs;
+using SwivelAcademyCourseManagement.Domain.Models;
+
+namespace SwivelAcademyCourseManagement.Domain.Maps
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<TeacherToRegisterDTO, Teacher>();
+            CreateMap<StudentToRegisterDTO, Teacher>();
+            CreateMap<StudentToUpdateDTO, Student>();
+            CreateMap<TeacherToUpdateDTO, Teacher>();
+            CreateMap<CourseToAddDTO, Course>();
+            CreateMap<CourseToUpdateDTO, Course>();
+        }
+    }
+}
